899bf8512b9589882862da546f73a075a172821c
Arthur L <arthur.lev44@gmail.com>
2021-03-03 14:42:08 +0100

    Structure app

---
+++ public/serviceWorker.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('pwa')
+    .then(cache => {
+      return cache.addAll([
+        '/',
+        '/serviceWorker.js',
+        '/index.html',
+        '/list.png',
+        'manifest.json'
+      ])
+      .then(() => self.skipWaiting())
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
