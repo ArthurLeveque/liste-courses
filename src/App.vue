919fc1846a7daea765f1832e82c6e56ff6a8058f
Maxouleloup <maxence_86@hotmail.fr>
2021-03-05 15:00:33 +0100

    création du composant liste de course

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,72 @@
 <template>
+  <div>
   <list-input></list-input>
+    <ul>
+        <courses-list class="articlesShow" v-for="(product, index) in list" :index="index" :name="product.name" :key="index" @delete-click="removeItem"></courses-list>
+    </ul>
+  </div>
 </template>
 
 <script>
-  import ListInput from "./components/ListInput";
+import ListInput from "./components/ListInput";
+import CoursesList from './components/CoursesList.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      ListInput
-    }
-  }
+export default {
+  name: 'App',
+  components: {
+    CoursesList,
+    ListInput,
+  },
+  data() {
+    return {
+      list: [
+        {
+          name: 'Abricot',
+        },
+
+        {
+          name: 'ananas',
+        },
+        
+        {
+          name: 'patates',
+        },
+        
+        {
+          name: 'courgettes',
+        },
+      ],
+    };
+  },
+
+  methods: {
+    removeItem(i) {
+    this.list.splice(i, 1)
+    },
+  },
+}
 </script>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-  }
 
-  #app {
-    position: relative;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+   background-color: rgb(36, 0, 94);
+   padding-left: 30px;
+   padding-right: 30px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  position: relative;
+  margin-top: 60px;
+}
 </style>
---
+++ src/components/CoursesList.vue
@@ -0,0 +1,44 @@
+<template>
+<li>
+    <div class="courses-list">
+        <p>{{ name }}</p>
+        <button @click="triggerDeleteClick">x</button>
+    </div>
+    <hr>
+</li>
+</template>
+
+<script>
+
+export default {
+    props : ['name', 'index'],
+    emits : ['deleteClick'],
+    methods : {
+       triggerDeleteClick() {
+         this.$emit('deleteClick', this.index)
+      },
+    },
+}
+</script>
+
+<style scoped>
+li {
+  list-style-type: none;
+  margin-bottom: 10px;
+}
+hr {
+  margin-top: 10px;
+}
+.courses-list {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+button {
+  margin-left: 15px;
+  font-size: 20px;
+  color: red;
+  background: none;
+  border: none;
+}
+</style>
