40b81a87931230584b331216286a20314cd62e95
Arthur L <arthur.lev44@gmail.com>
2021-03-05 14:40:15 +0100

    Ajout input

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <list-input></list-input>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ListInput from "./components/ListInput";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      ListInput
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    position: relative;
+  }
 </style>
---
+++ src/components/ListInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input v-model="name" @keypress.enter="addItem" type="text">
+    <button @click="addItem">+</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    emits: ['name'],
+    data() {
+      return {
+        name: '',
+      }
+    },
+    methods: {
+      updateUsername() {
+        this.$emit('name', this.nameValue);
+      }
+    },  
+  }
+</script>
+
+<style scoped>
+  div{
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background: #ebebeb;
+    padding: 10px 0;
+  }
+  input{
+    width: 70%;
+    padding: 10px;
+  }
+  button{
+    width: 20%;
+    padding: 10px;
+  }
+</style>
